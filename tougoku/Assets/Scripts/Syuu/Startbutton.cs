39512b347c3d75d918ab21e88b60b75cd3d504d5
kazeyuki1876 <32504095+kazeyuki1876@users.noreply.github.com>
2019-05-10 09:10:17 +0900

    タイトル　作業（10％）

---
+++ tougoku/Assets/Scripts/Syuu/Startbutton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Startbutton : MonoBehaviour
+{
+    void Start() {
+        this.GetComponent<Button>().onClick.Addlistener(Clickme);
+    }
+    void Clickme()
+    {
+        Debug.Log("開始ボタン");
+    }
+}
